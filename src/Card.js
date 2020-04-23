9b6aef97f78746c1657f0590b99cbd512412f35e
Wanda Antoine <wanda@thinkful.com>
2020-04-23 18:07:32 -0400

    implemented delete and add random card button

--- src/Card.js
+++ src/Card.js
@@ -5,10 +5,14 @@ function Card(props) {
     return (
         <div className="Card">
             <button type="button">delete</button>
+            onClick={() => props.onClickDelete(props.id)}
             <h3>{props.title}</h3>
             <p>{props.content}</p>
         </div>
     );
 }
 
+Card.propTypes = {
+    onClickDelete: () => {}
+  }
 export default Card;
--- src/List.js
+++ src/List.js
@@ -12,19 +12,29 @@ function List(props) {
             </header>
 
             <div className="List-cards">
-                    {props.cards.map(card =>
-                        <Card
-                            key={card.id}
-                            title={card.title}
-                            content={card.content}
-                        />
-                    )}
-                    <button type="button" className="List-add-button">
+                {props.cards.map(card =>
+                    <Card
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        content={card.content}
+                        onClickDelete={props.onClickDelete}
+                    />
+                )}
+                <button
+                    type="button"
+                    className="List-add-button"
+                    onClick={() => props.onClickAdd(props.id)}
+                >
                     + Add Random Card
                     </button>
-                </div>
+            </div>
         </section>
     );
+}
+
+List.defaultProps = {
+    onClickAdd: () => { },
 }
 
 export default List;
